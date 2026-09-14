56cf2c7722006564afc681af6de30cfee1c3f376
shanukumar2412 <kumarshanu769787@gmal.com>
2026-09-14 21:52:08 +0530

    Configure base path and update project

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/INDIAN-ART-FORM-ASSIGNMENT-2/',
+  base: './',
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
